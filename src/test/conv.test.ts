cd8d8851ddac0ab6a1e3001537ff7dff79ffb7cb
Vladislav Veluga <vladislav805@yandex.com>
2019-10-01 00:23:25 +0300

    Добавление поле со статистикой сжатия, а также испрвлен баг с архивированием длинных текстов (uint8 был заменен на uint16)

--- src/test/conv.test.ts
+++ src/test/conv.test.ts
@@ -1,14 +1,8 @@
 import {} from 'jest';
-import { StringToUInt8 as s2i, UInt8ToString as i2s } from '../util/conv';
+import { StringToUInt16 as s2i, UInt16ToString as i2s } from '../util/conv';
 
 it('Обратимость', () => {
-    const text = 'Test here is more text';
+    const text = 'Test here is more text или даже русский!"@@@#-1`';
 
     expect(i2s(s2i(text))).toBe(text);
-});
-
-it('Из-за uint8 русские символы не будут сохраняться (поскольку код символа вне 255)', () => {
-    const text = 'Русский';
-
-    expect(i2s(s2i(text))).not.toBe(text);
 });
